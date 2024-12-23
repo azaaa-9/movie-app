ee095dd7edd449b84b94cf165ff050c26d31171d
azaaa-9 <az95342030@gmail.com>
2024-12-23 18:05:11 +0800

    movie-app started

---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,6 @@
+ export const Footer = ()=> {
+    return (
+        <div className="bg-green-400 h-[400px]">hello</div>
+    )
+
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Footer } from "./_components/footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +27,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="min-h-screen">{children}</main>
+        <Footer />
       </body>
     </html>
   );
