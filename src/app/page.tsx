3bafbcfec03597a91665bf292b2d28fb513717be
azaaa-9 <az95342030@gmail.com>
2024-12-24 17:32:32 +0800

    movies data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+"use client"
 import Image from "next/image";
-
+import { useEffect } from "react";
+export const API_KEY = 'f39690f9830ce804b7894ac1def4f7e9';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzk2OTBmOTgzMGNlODA0Yjc4OTRhYzFkZWY0ZjdlOSIsIm5iZiI6MTczNDk0OTM3MS43NDIsInN1YiI6IjY3NjkzOWZiYzdmMTcyMDVkMTBiMGIxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2r2TerxSJdZGmGVSLVDkk6nHT0NPqY4rOcxHtMNt0aE',
+  },
+};
 export default function Home() {
+ fetch(
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+  options
+ )
+ .then((res) => res.json())
+ .then((data) => {
+  console.log(data);
+ });
   return (
     <div >
-      movie-app-----
+      
     </div>
   );
 }
