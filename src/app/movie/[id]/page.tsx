d97c49b65a2e1196a8c4a6abd4ace0081b0e01a5
azaaa-9 <az95342030@gmail.com>
2024-12-26 18:18:54 +0800

    movie/[id] +img

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -4,21 +4,26 @@ import { Options } from "../../_components/constants/type"
 type Props={
     params: {
         id: string;
+        
     }
 }
+
 export default async function Page ({ params }: Props) {
     const response = await fetch(
         `https://api.themoviedb.org/3/movie/${params.id}`,
   Options
     ); 
-
-    const data = await response.json();
+   const data = await response.json(); 
+   const imgPath = data?.poster_path ?? data?.backdrop_path;
+   const src = imgPath
+   ? `https://image.tmdb.org/t/p/w154/${imgPath}`
+   : `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFtIiwbQLKvRRQn_06612_CSC84SsKQTbvoQ&s`;
     return(
         <div>
           <Header />
             <p className="text-2xl font-bold">{data.title}</p>
             <p>{data.release_date}</p>
-            <p>{data.poster_path}</p>
+            <img src={src} />
             <p>{data.overview}</p>
             
             </div>
